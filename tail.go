d8889ae69fb2277b3161a357ecf0f327b56013d2
Zander Hill <zander@xargs.io>
2017-03-15 11:11:56 -0700

    Upgrade GTM to latest to avoid data race condition
    See: https://github.com/rwynn/gtm/commit/22eec696103260d143b92a08ef06b45173c072a2

--- tail.go
+++ tail.go
@@ -164,14 +164,6 @@ func (t *Tailer) Read() {
 				if c := t.fan[key]; c != nil {
 					collection := t.config[db].Collections[coll]
 					o := Statement{collection}
-					// Make copy to avoid situation where op has reference to shared underlying .Data map
-					// Otherwise we run into errors with concurrent read/write map access
-					newData := make(map[string]interface{})
-					for k, v := range op.Data {
-						newData[k] = v
-					}
-					log.WithField("operation data", newData).Debug("Received from GTM")
-					op.Data = newData
 					c <- EnsureOpHasAllFields(op, o.mongoFields())
 				} else {
 					t.counters.skipped.Incr(1)
